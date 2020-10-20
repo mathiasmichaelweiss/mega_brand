d09587cb1492f1ad8fff8ebdd22e17715ad28697
Mathias <mathiasmichaelweiss@gmail.com>
2020-10-20 15:20:25 +0200

    create anchors and do this smooth animation

--- js/script.js
+++ js/script.js
@@ -47,6 +47,25 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     };
 
+    // anchors
+
+    const anchors = document.querySelectorAll('a[href*="#"]');
+    console.log(anchors);
+
+
+    for (let anchor of anchors) {
+        anchor.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            const blockID = anchor.getAttribute('href').substr(1);
+
+            document.getElementById(blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    }
+
     // Shop Class
 
     class Shop {
