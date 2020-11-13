df43a90e475d54a09eb6a12c44ab637b4b719e4c
Mathias <mathiasmichaelweiss@gmail.com>
2020-11-13 18:59:09 +0100

    make menu horison on scroll 0_4

--- js/index.js
+++ js/index.js
@@ -57,7 +57,8 @@ window.addEventListener('DOMContentLoaded', () => {
             const menu = document.querySelector('.container__menu'),
                 nav = document.querySelector('.nav__position-active'),
                 containerNav = document.querySelector('.container__nav'),
-                navMouse = document.querySelectorAll('.nav-mouse');
+                navMouse = document.querySelectorAll('.nav-mouse'),
+                navMouseHorison = document.querySelectorAll('.nav-mouse-horison');
 
             if (window.pageYOffset > 138) {
                 document.querySelector('.burger__icon').classList.remove('burger__icon-active');
@@ -67,6 +68,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 containerNav.classList.add('horison-nav-container');
                 navMouse.forEach(item => {
                     item.classList.add('nav-mouse-horison');
+                    item.classList.remove('nav-mouse');
                 });
             } else {
                 document.querySelector('.burger__icon').classList.add('burger__icon-active');
@@ -76,6 +78,10 @@ window.addEventListener('DOMContentLoaded', () => {
                 containerNav.classList.remove('horison-nav-container');
                 navMouse.forEach(item => {
                     item.classList.remove('nav-mouse-horison');
+                    item.classList.add('nav-mouse');
+                });
+                navMouseHorison.forEach(item => {
+                    item.classList.add('nav-mouse');
                 });
             }
 
