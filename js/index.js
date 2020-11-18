e80889fbd53a53062713c828301844de31f3afe1
Mathias <mathiasmichaelweiss@gmail.com>
2020-11-18 15:15:54 +0100

    preload logo

--- js/index.js
+++ js/index.js
@@ -2,12 +2,6 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
-
-    window.onload = function () {
-        const logo = document.querySelector('.m-logo');
-        logo.style.backgroundImage = "url(../img/logo_mega_once.gif)";
-    }
-
     const closeBtn = document.querySelectorAll('.close__btn');
 
     function animateBtn(btn) {
@@ -903,8 +897,14 @@ window.addEventListener('DOMContentLoaded', () => {
     preloadImages(
         'img/charity_slider_image/blago_1.jpg',
         'img/charity_slider_image/blago_2.jpg',
-        'img/charity_slider_image/blago_3.jpg'
+        'img/charity_slider_image/blago_3.jpg',
+        'img/logo_mega_once.gif'
     );
+
+    window.onload = function () {
+        const logo = document.querySelector('.m-logo');
+        logo.style.backgroundImage = "url(../img/logo_mega_once.gif)";
+    }
 
     const images = [
         'rgba(0, 0, 0, 0.2)  url(img/charity_slider_image/blago_1.jpg)',
