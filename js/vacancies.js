274066b6a881e53f388e00d75fa63a13611b7c78
Mathias <mathiasmichaelweiss@gmail.com>
2020-11-22 11:29:11 +0100

    make accordion on vacancies page

--- js/vacancies.js
+++ js/vacancies.js
@@ -1,17 +1,16 @@
 'use strict';
 
 const vacanciesItem = document.querySelectorAll('.vacancies-item'),
-    smallContainer = document.querySelector('.small-container'),
-    width = window.getComputedStyle(smallContainer).width,
     openVacancies = document.querySelectorAll('.full-vacan');
+console.log(vacanciesItem);
 
-console.log(width);
-
-vacanciesItem.forEach(item => {
-    openVacancies.forEach(item2 => {
-        item2.style.width = width;
-        item.addEventListener('click', () => {
-            item2.style.top = '860px';
+vacanciesItem.forEach((item, i) => {
+    item.addEventListener('click', (e) => {
+        openVacancies.forEach(item2 => {
+            item2.classList.remove('vac__opened');
         });
+
+        /*         item.classList.add('vac__shadow'); */
+        openVacancies[i].classList.add('vac__opened');
     });
 });
