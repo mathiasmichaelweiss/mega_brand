02f38d14588d76554f8ee338596f5f7bae03ed75
Mathias <mathiasmichaelweiss@gmail.com>
2020-11-22 12:53:19 +0100

    finish accordion

--- js/vacancies.js
+++ js/vacancies.js
@@ -1,16 +1,23 @@
 'use strict';
 
 const vacanciesItem = document.querySelectorAll('.vacancies-item'),
+    vacanciesItemContainer = document.querySelectorAll('.vacan-item-container'),
     openVacancies = document.querySelectorAll('.full-vacan');
-console.log(vacanciesItem);
+
 
 vacanciesItem.forEach((item, i) => {
     item.addEventListener('click', (e) => {
-        openVacancies.forEach(item2 => {
+        vacanciesItemContainer.forEach(item2 => {
             item2.classList.remove('vac__opened');
         });
 
-        /*         item.classList.add('vac__shadow'); */
-        openVacancies[i].classList.add('vac__opened');
+        vacanciesItemContainer[i].classList.add('vac__opened');
+        vacanciesItem[i].classList.add('vac__active');
+
+        document.querySelectorAll('.vac__active').forEach((item3, i3) => {
+            item3.addEventListener('click', () => {
+                vacanciesItemContainer[i].classList.toggle('vac__opened');
+            });
+        });
     });
 });
