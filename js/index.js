6a6699d5af2d132f62feae34d643f95109b9cc23
Mathias <mathiasmichaelweiss@gmail.com>
2020-11-27 13:43:28 +0100

    fix bag with menu 0_6

--- js/index.js
+++ js/index.js
@@ -49,15 +49,15 @@ window.addEventListener('DOMContentLoaded', () => {
         function displayNoneOnTIme() {
             if (document.querySelector('.container').clientWidth < 421) {
                 document.querySelectorAll('.nav-mouse').forEach(item => {
-                    window.setTimeout(() => {
-                        item.style.display = 'none';
-                        document.querySelector('.container__nav-mobile').style.display = 'none';
-                    }, 300);
+                    /* window.setTimeout(() => { */
+                    item.style.display = 'none';
+                    document.querySelector('.container__nav-mobile').style.display = 'none';
+                    /* }, 300); */
                 });
             }
         }
 
-        function displayNone() {
+        function displayBlock() {
             if (document.querySelector('.container').clientWidth < 421) {
                 document.querySelectorAll('.nav-mouse').forEach(item => {
                     item.style.display = 'inline-block';
@@ -69,14 +69,19 @@ window.addEventListener('DOMContentLoaded', () => {
         burgerLocation.addEventListener('click', () => {
             burgerIcon.classList.toggle('burger__icon-active');
             if (navActivate.classList.contains('nav__position')) {
-                navActivate.classList.remove('nav__position');
-                navActivate.classList.add('nav__position-active');
-                burgerIconMobile.classList.add('burger__icon-mobile-white');
-                displayNone();
+                window.setTimeout(() => {
+                    navActivate.classList.remove('nav__position');
+                    navActivate.classList.add('nav__position-active');
+                    burgerIconMobile.classList.add('burger__icon-mobile-white');
+                }, 300);
+                displayBlock();
             } else {
-                burgerIconMobile.classList.remove('burger__icon-mobile-white');
-                navActivate.classList.add('nav__position');
+                window.setTimeout(() => {
+                    navActivate.classList.add('nav__position');
+                    navActivate.classList.remove('nav__position-active');
+                }, 300);
                 navActivate.classList.remove('nav__position-active');
+                burgerIconMobile.classList.remove('burger__icon-mobile-white');
                 displayNoneOnTIme();
             }
         });
