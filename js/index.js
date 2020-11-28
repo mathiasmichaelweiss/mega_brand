7749383e2ce8756e66be136892eb3e7920ee6362
Mathias <mathiasmichaelweiss@gmail.com>
2020-11-28 10:56:30 +0100

    fix overflow-x on main page

--- js/index.js
+++ js/index.js
@@ -38,7 +38,7 @@ window.addEventListener('DOMContentLoaded', () => {
             navActivate = document.querySelector(navActive),
             burgerIconMobile = document.querySelector('.burger__icon-mobile');
 
-        if (document.querySelector('.container').clientWidth < 421) {
+        if (document.querySelector('.container').clientWidth < 750) {
             burgerIcon.classList.add('burger__icon-active');
             document.querySelector('.container__nav-mobile').style.display = 'none';
         } else {
@@ -47,7 +47,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
 
         function displayNoneOnTIme() {
-            if (document.querySelector('.container').clientWidth < 421) {
+            if (document.querySelector('.container').clientWidth < 750) {
                 document.querySelectorAll('.nav-mouse').forEach(item => {
                     /* window.setTimeout(() => { */
                     item.style.display = 'none';
@@ -58,7 +58,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
 
         function displayBlock() {
-            if (document.querySelector('.container').clientWidth < 421) {
+            if (document.querySelector('.container').clientWidth < 750) {
                 document.querySelectorAll('.nav-mouse').forEach(item => {
                     item.style.display = 'inline-block';
                     document.querySelector('.container__nav-mobile').style.display = 'block';
@@ -829,7 +829,7 @@ window.addEventListener('DOMContentLoaded', () => {
         tabHeader = document.querySelector('.tabheader');
 
     const pageContainer = document.querySelector('.container');
- 
+
 
     addScroll(newsScroll, newsCointainer);
     activateElem(itemBlock, 'container__news__item-active');
